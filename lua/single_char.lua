75b3b3c4d3d891c4b408f9ea021edf1e1d699e07
Vitta <wanglikun@shinetechchina.com>
2023-08-21 10:19:44 +0800

    更新词库

--- lua/single_char.lua
+++ lua/single_char.lua
@@ -1,19 +1,24 @@
 --[[
 single_char_filter: 候选项重排序，使单字优先
---]]
+--]] 
+
+
+local function is_chinese(char)
+  return string.match(char, "[\\u4e00-\\u9fff]") ~= nil 
+end
 
 local function filter(input)
-   local l = {}
-   for cand in input:iter() do
-      if (utf8.len(cand.text) == 1) then
-	 yield(cand)
-      else
-	 table.insert(l, cand)
-      end
-   end
-   for i, cand in ipairs(l) do
-      yield(cand)
-   end
+    local l = {}
+    for cand in input:iter() do
+        if (utf8.len(cand.text) == 1)  then
+            yield(cand)
+        else
+            table.insert(l, cand)
+        end
+    end
+    for i, cand in ipairs(l) do
+        yield(cand)
+    end
 end
 
 return filter
