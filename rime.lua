2db8a084f63416c07282a428531ee5210cac1761
Vitta <wanglikun@shinetechchina.com>
2023-12-13 18:22:07 +0800

    英语模块化

--- rime.lua
+++
@@ -1,7 +0,0 @@
-
-local english = require("xmjd6.english")()
-english_processor = english.processor
-english_segmentor = english.segmentor
-english_translator = english.translator
-english_filter = english.filter
-english_filter0 = english.filter0
