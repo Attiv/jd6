8341c8ec6239d142b22163069183671083c7113b
Vitta <wanglikun@shinetechchina.com>
2023-12-18 00:15:15 +0800

    更新打包脚本，把tofu 设置为true

--- build.sh
+++ build.sh
@@ -13,11 +13,22 @@ fi
 # create the zip file
 zip -r jd6.zip . -x "jd6_build.zip" -x "build/*" -x ".git/*" -x ".gitignore*" -x ".history/*" -x "build.sh" -x "兰佬转词库.zip" -x "词库转换键道.zip" -x "qui.py" -x ".scripts/*" -x "chongfu.py" -x ".build_back/*" -x ".build_back" -x "zhuyin.py"
 
+
+file_path="user.yaml"
+
+if [ -f "$file_path" ]; then
+    # 使用sed替换tofu的值
+    sed -i '/^ *tofu:/s/false/true/' "$file_path"
+    echo "tofu的值已设置为true"
+else
+    echo "文件 $file_path 不存在"
+fi
+
 # move xmjd6enre.reverse.bin to build folder
-mv build/xmjd6enre.reverse.bin build/
+# mv build/xmjd6enre.reverse.bin build/
 
 # add xmjd6enre.reverse.bin to the existing zip file
-zip -ur jd6.zip build/xmjd6enre.reverse.bin
+# zip -ur jd6.zip build/xmjd6enre.reverse.bin
 
 
 cp jd6.zip /Users/wanglikun/Library/Mobile\ Documents/com~apple~CloudDocs/jd6.zip
