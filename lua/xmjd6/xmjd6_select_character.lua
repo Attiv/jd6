8e0fff34be52d9420d03066336d0844b0ca93842
vitta <vittawlk@gmail.com>
2024-03-31 22:52:57 +0800

    update

--- lua/xmjd6/xmjd6_select_character.lua
+++ lua/xmjd6/xmjd6_select_character.lua
@@ -56,20 +56,13 @@ local function select_character(key, env)
     local first_key = config:get_string('key_binder/select_first_character')
     local last_key = config:get_string('key_binder/select_last_character')
 
-
-    if context:has_menu() and context:get_selected_candidate().text ~= '「' then
+    if context:has_menu() and context:get_selected_candidate().text ~= '' then
         if (key:repr() == first_key) then
             if (context:get_selected_candidate().text) then
                 engine:commit_text(utf8_sub(context:get_selected_candidate().text, 1, 1))
                 context:clear()
             end
-            return 1 -- kAccepted
-        elseif (key:repr() == last_key) then
-            if (context:get_selected_candidate().text) then
-                engine:commit_text(utf8_sub(context:get_selected_candidate().text, -1, -1))
-                context:clear()
-            end
-            return 1 -- kAccepted
+            return 1
         end
     end
 
