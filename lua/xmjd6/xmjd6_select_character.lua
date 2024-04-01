ffd6cf41912c722bb92f052584602464fbe4eaae
vitta <vittawlk@gmail.com>
2024-04-01 10:32:59 +0800

    更新以词定字

--- lua/xmjd6/xmjd6_select_character.lua
+++ lua/xmjd6/xmjd6_select_character.lua
@@ -2,6 +2,7 @@
 -- 来源 https://github.com/BlindingDark/rime-lua-select-character
 -- 删除了默认按键 [ ]，和方括号翻页冲突，需要在 key_binder 下指定才能生效
 -- 20230526195910 不再错误地获取commit_text，而是直接获取get_selected_candidate().text。
+-- http://lua-users.org/lists/lua-l/2014-04/msg00590.html
 local function utf8_sub(s, i, j)
     i = i or 1
     j = j or -1
@@ -45,25 +46,46 @@ local function utf8_sub(s, i, j)
     end
 end
 
+local function first_character(s)
+    return utf8_sub(s, 1, 1)
+end
+
+local function last_character(s)
+    return utf8_sub(s, -1, -1)
+end
+
+local function get_commit_text(context, fun)
+    local candidate_text = context:get_selected_candidate().text
+    local selected_character = fun(candidate_text)
+
+    context:clear_previous_segment()
+    local commit_text = context:get_commit_text()
+
+    context:clear()
+
+    return commit_text .. selected_character
+end
+
 local function select_character(key, env)
     local engine = env.engine
     local context = engine.context
-    local commit_text = context:get_commit_text()
     local config = engine.schema.config
 
-    -- local first_key = config:get_string('key_binder/select_first_character') or 'bracketleft'
-    -- local last_key = config:get_string('key_binder/select_last_character') or 'bracketright'
-    local first_key = config:get_string('key_binder/select_first_character')
-    local last_key = config:get_string('key_binder/select_last_character')
+    local first_key = config:get_string('key_binder/select_first_character') or 'bracketleft'
+    local last_key = config:get_string('key_binder/select_last_character') or 'bracketright'
 
-    if context:has_menu() and context:get_selected_candidate().text ~= '' then
-        if (key:repr() == first_key) then
-            if (context:get_selected_candidate().text) then
-                engine:commit_text(utf8_sub(context:get_selected_candidate().text, 1, 1))
-                context:clear()
-            end
-            return 1
-        end
+    local commit_text = context:get_commit_text()
+
+    if (key:repr() == first_key and commit_text ~= "") then
+        engine:commit_text(get_commit_text(context, first_character))
+
+        return 1 -- kAccepted
+    end
+
+    if (key:repr() == last_key and commit_text ~= "") then
+        engine:commit_text(get_commit_text(context, last_character))
+
+        return 1 -- kAccepted
     end
 
     return 2 -- kNoop
